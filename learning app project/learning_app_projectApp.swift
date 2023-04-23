cbcf6451d12802771dd9f5487c2f351faab71de2
marcel <marcel.maciaszek@icloud.com>
2023-04-23 22:34:08 +0200

    Initial Commit

---
+++ learning app project/learning_app_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_app_projectApp.swift
+//  learning app project
+//
+//  Created by Marcel Maciaszek on 23/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct learning_app_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
