d8837451e1e5bf01127af6078f636a0fc223532f
marcel <marcel.maciaszek@icloud.com>
2023-04-25 18:40:34 +0200

    HomeView and HomeViewRow changes

---
+++ learning app project/Views/HomeViewRow.swift
@@ -0,0 +1,77 @@
+//
+//  HomeViewRow.swift
+//  learning app project
+//
+//  Created by Marcel Maciaszek on 25/04/2023.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    
+    var image: String
+    var title: String
+    var description: String
+    var count: String
+    var time: String
+    
+    
+    var body: some View {
+        ZStack {
+            
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+            
+            HStack {
+                
+                //Image
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                
+                Spacer()
+                
+                //Text
+                VStack (alignment: .leading, spacing: 10) {
+                    Text(title)
+                        .bold()
+                    Text(description)
+                        .padding(.bottom, 20)
+                        .font(.caption)
+                    
+                    HStack{
+                        //number of lessons
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(count)
+                            .font(Font.system(size: 11))
+                            .bold()
+                        
+                        //time
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(time)
+                            .font(Font.system(size: 11))
+                            .bold()
+
+                        
+                    }
+                }
+                .padding(.leading, 20)
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+}
+
+struct HomeViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewRow(image: "swift", title: "Learn Swift", description: "some description", count: "10 lessons", time: "2 Hours")
+    }
+}
