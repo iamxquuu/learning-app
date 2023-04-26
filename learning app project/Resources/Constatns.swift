e8a9624d5dfe6dbd901c004443e95b23c402e472
marcel <marcel.maciaszek@icloud.com>
2023-04-26 17:56:28 +0200

    new View videos functionality button to the next lesson

---
+++ learning app project/Resources/Constatns.swift
@@ -0,0 +1,13 @@
+//
+//  Constatns.swift
+//  learning app project
+//
+//  Created by Marcel Maciaszek on 26/04/2023.
+//
+
+import Foundation
+
+struct Constants {
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+}
+
--- learning app project/ViewModel/ContentModel.swift
+++ learning app project/ViewModel/ContentModel.swift
@@ -14,6 +14,9 @@ class ContentModel: ObservableObject {
     var currentModuleIndex = 0
     
     
+    @Published var currentLesson: Lesson?
+    var currentLessonIndex = 0
+    
     var styleData: Data?
     
     init() {
@@ -65,5 +68,30 @@ class ContentModel: ObservableObject {
         
         currentModule = modules[currentModuleIndex]
         
+    }
+    
+    func beginLesson(_ lessonIndex:Int) {
+        if lessonIndex < currentModule!.content.lessons.count {
+            currentLessonIndex = lessonIndex
+        }
+        else{
+            currentLessonIndex = 0
+        }
+        currentLesson = currentModule!.content.lessons[currentLessonIndex]
+    }
+    func nextLesson() {
+        currentLessonIndex += 1
+        
+        if currentLessonIndex < currentModule!.content.lessons.count {
+            currentLesson = currentModule!.content.lessons[currentLessonIndex]
+        }
+        else{
+            currentLesson = nil
+            currentLessonIndex = 0
+        }
+    }
+    
+    func hasNextLesson() -> Bool {
+        return (currentLessonIndex + 1 < currentModule!.content.lessons.count)
     }
 }
